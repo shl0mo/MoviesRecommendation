28fad266fb2514aafcaf108d58571edbd0695482
shl0mo <salomao.dacostacruz@gmail.com>
2023-11-22 07:27:36 -0400

    RecommendationsReq - init #

--- src/routes/Recommendations/index.tsx
+++ src/routes/Recommendations/index.tsx
@@ -19,54 +19,29 @@ interface RecommendationsReqProps {
 
 const moviesArray = [];
 export function RecommendationsReq (props: RecommendationsReqProps) {
-	// const favoritesArray = JSON.parse(store.getState().favoritesArray);
 	const [recommendedMovies, setRecommendedMovies] = useState([]);
 	
-	
-	/*if (favoritesArray.length === 0) {
-		return (
-			<div style={{display: 'flex', alignSelf: 'center'}}>
-				<h1 >Nenhum filme favoritado</h1>
-			</div>
-		)
-	}*/
-	
+	async function MovieDetails(movieId: number) {
+                const movieDetail = await GetMovieDetails(movieId);
+                navigate("/details", { state: { movieDetail } });
+        }
 	
 	async function GetRecommendation (movieId: number): TheMovieDB {
 		GetRecommendations(movieId).then((moviesObj) => {
 			moviesObj.results.map((movie) => {
 				moviesArray.push([
                                         <MoviePosterBox key={movie.id}>
-                                                <MoviePosterImage src={`${import.meta.env.VITE_APP_BASE_URL_IMAGEM}/${movie.poster_path}`} onClick={() => console.log('s')} />
+                                                <MoviePosterImage src={`${import.meta.env.VITE_APP_BASE_URL_IMAGEM}/${movie.poster_path}`} onClick={() => MovieDetails(props.movieId)} />
                                         </MoviePosterBox>
 				])
 			})
 			setRecommendedMovies(moviesArray);
 		});
-		/*setRecommendedMovies([
-                                                <MoviePosterBox key={'a'}>
-							<h1>Olá</h1>
-                                                </MoviePosterBox>
-		]);*/
 	}
 	
 	useEffect(() => {
 		GetRecommendation(props.movieId);
-	}, []);
-
-	useEffect(() => {
-		console.log(recommendedMovies);
-		/*if (recommendedMovies.length == 1) {
-			recommendedMovies?.results.map((movie) => {
-				/*moviesArray.push(
-                                                <MoviePosterBox key={movie.id}>
-                                                        <MoviePosterImage src={`${import.meta.env.VITE_APP_BASE_URL_IMAGEM}/${movie.poster_path}`} onClick={() => MovieDetails(movie.id)} />
-                                                </MoviePosterBox>
-				)
-			})
-		}*/
-	}, [recommendedMovies]);
-	
+	}, []);	
 
 	return (
 		<CategoryContainer>
