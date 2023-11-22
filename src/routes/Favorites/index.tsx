1a8a10b1393b9d280c49a9bb58ecdc4420d049c8
shl0mo <salomao.dacostacruz@gmail.com>
2023-11-22 00:18:25 -0400

    addFavorite() #

--- src/routes/Favorites/index.tsx
+++ src/routes/Favorites/index.tsx
@@ -1,3 +1,5 @@
+import store from '../../store';
+
 export function Favorites () {
-	
+	console.log(store.getState());
 }
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,19 +1,42 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+
+const favoritesArrayName = 'favoritesArray';
+setLocalStorageFavoritesArray();
+
 const Slice = createSlice({
 	name: 'movies',
 	initialState: {
-		favoritesArray: [],
+		favoritesArray: localStorage.getItem(favoritesArrayName),
 	},
 	reducers: {
-		addFavorite: (state, { payload }) => {
-			state.favoritesArray = [
-				...state.favoritesArray,
+		addFavorite (state, { payload }) {
+			let favoritesArray = JSON.parse(state.favoritesArray);
+			const movieId = payload.movieId;
+			let alreadyAdded = false;
+			for (let movieInfo of favoritesArray) {
+				if (movieInfo.movieId === movieId) alreadyAdded = true;
+			}
+			if (alreadyAdded) {
+				alert('O filme já consta na lista de favoritos');
+				return;
+			} 
+			favoritesArray = [
+				...favoritesArray,
 				payload
 			];
+		       	localStorage.setItem(favoritesArrayName, JSON.stringify(favoritesArray));
+			state.favoritesArray = localStorage.getItem(favoritesArrayName);
+			alert('Filme adicionado na lista de favoritos com sucesso');
 		}
 	}
 })
+
+function setLocalStorageFavoritesArray() {
+	console.log('setando o array');
+	if (localStorage.getItem(favoritesArrayName) === null)
+		localStorage.setItem(favoritesArrayName, JSON.stringify([]));
+}
 
 export const { addFavorite } = Slice.actions;
 export default Slice.reducer;
